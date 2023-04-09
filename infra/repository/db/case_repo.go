d350dac23a2577a3858858af8c4ab71e35487ac8
toxb11 <xu_tong@outlook.com>
2023-04-09 21:50:26 +0800

    fix5

--- infra/repository/db/case_repo.go
+++ infra/repository/db/case_repo.go
@@ -45,12 +45,11 @@ func (r *caseRepositoryImpl) MGetCase(ctx context.Context, queryParam *model.Cas
 	if queryParam.QueryTime != nil {
 		begin := time.Unix(queryParam.QueryTime.BeginTime, 0)
 		end := time.Unix(queryParam.QueryTime.EndTime, 0)
-		logrus.Infof("[MGetCase Repo] begin: %v, end: %v\n", xjson.ToString(begin), xjson.ToString(end))
-		CaseDBHandler.Table(caseEntity.TableName()).Where("create_time BETWEEN ? AND ?", begin.Format(time.DateTime), end.Format(time.DateTime))
+		CaseDBHandler.Table(caseEntity.TableName()).Where("create_time BETWEEN ? AND ?", begin.Format(time.DateTime), end.Format(time.DateTime)).Find(&caseEntityList)
 		return dbResHandler(ctx, caseEntityList, doList)
 	}
 	if queryParam.CriminalId != nil {
-		CaseDBHandler.Table(caseEntity.TableName()).Where("where criminal_id = ?", *queryParam.CriminalId)
+		CaseDBHandler.Table(caseEntity.TableName()).Where("where criminal_id = ?", *queryParam.CriminalId).Find(&caseEntityList)
 		return dbResHandler(ctx, caseEntityList, doList)
 	}
 	return doList, nil
