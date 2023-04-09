ce77ad2685e94bd2b78bfc667279d4956679eb74
toxb11 <xu_tong@outlook.com>
2023-04-09 21:53:50 +0800

    fix6

--- infra/ai/client.go
+++ infra/ai/client.go
@@ -15,7 +15,7 @@ var OpenAIClient *openai.Client
 
 const (
 	openaiAPIKey = "T0wcbTSzr07H2ElfhT9kyJ0T3BlbkFJy6CMb3wktQQDm0YUhsk-"
-	gpt4Model    = "gpt-4"
+	gpt4Model    = "gpt-4-32k"
 	gpt35Model   = "gpt-3.5-turbo"
 )
 
@@ -36,7 +36,7 @@ func GeneratePromptBySum(ctx context.Context, prevMsg string, curMsg string) (st
 		return curMsg, nil
 	}
 	req := openai.ChatCompletionRequest{
-		Model: gpt35Model,
+		Model: gpt4Model,
 		Messages: []openai.ChatCompletionMessage{{
 			Role:    "user",
 			Content: fmt.Sprintf("下面两句话都是关于人脸的描述，请将这两句合并。\n第一句：%v\n第二句：%v\n", curMsg, prevMsg),
@@ -54,7 +54,7 @@ func GeneratePromptBySum(ctx context.Context, prevMsg string, curMsg string) (st
 
 func GetPromptDescribeTags(ctx context.Context, prompt string) (*model.FaceTag, error) {
 	req := openai.ChatCompletionRequest{
-		Model: gpt35Model,
+		Model: gpt4Model,
 		Messages: []openai.ChatCompletionMessage{{
 			Role:    "user",
 			Content: fmt.Sprintf("请分析下面句话是否有形容到一个人的年纪，性别，身体体型，脸型，头发样式，头发颜色，是否有胡须，眉毛样式，眉毛方向，眼睛形状，眉眼间距，是否双眼皮、是否有眼袋，鼻子形状，鼻眼间距，法令纹，皱纹，嘴巴大小,嘴唇厚薄,神态描述。请使用只有一层的英文json结构表示哪些属性被形容到（1代表被形容到），哪些属性没有被形容到（2代表没有被形容到）。我只需要json结构，不需要文本说明。\n%v", prompt),
